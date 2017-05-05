fe779508b84203e0f4022203902890e7dd02d7df
Rohit Mangla <mangla.rohit730@gmail.com>
2017-05-05 15:14:32 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -10,9 +10,15 @@ var infowindow;
 // Intialize markers as NULL 
 var markers = [];
 
+var locations = [];
+
 var metadata;
 	
-
+function mapError() {
+	console.log( "Erro come here");
+	appViewModel.error( true );
+	appViewModel.errorMessage("Could't load the map")
+}
 function fetchRestaurant() {
 	$.ajax({
 		url:'https://api.foursquare.com/v2/venues/search?v=20161016&ll=19.0760%20%2C%2072.8777&query=restaurant&intent=checkin&client_id=3RL5552F0F4CTDGNDKA5CFBQDTT2GOOVEYJHDK4VEYOD0GXW&client_secret=UBS24IQJ3PSGDPYSIYJ0JTPSBN4EFEV0EHI4PSEQJUOFYXCA',
@@ -20,24 +26,48 @@ function fetchRestaurant() {
 	}).done( function( response ) {
 		metadata = response.response.venues;
 		//console.log( metadata );
- 		for( var i = 0 ; i < metadata.length && i < 10	 ; i++ ) {
+ 		for( var i = 0 ; i < metadata.length; i++ ) {
  			//console.log( metadata[ i ]  );
  			var lat = metadata[ i ].location.lat;
  			var lng = metadata[ i ].location.lng;
+ 			var l = {
+ 				title : metadata[ i ].name,
+ 				position : { lat , lng }
+ 			};
+ 			locations.push( l );
  			var marker = new google.maps.Marker({
  				position: { lat , lng },
  				title: metadata[  i  ].name,
- 				animation : google.maps.Animation.DROP,
- 				id: i ,
  				phoneno :metadata[i].contact.phone,
- 				address : metadata[i].location.address
+ 				address : metadata[i].location.address,
+ 				animation : google.maps.Animation.DROP
  			});
+
  			markers.push( marker );
  			marker.addListener('click' , function() {
  				addInfoWindow( this , infowindow );
  			});
  		}
+ 		appViewModel.init();
+ 		//console.log( markers.length );
+	}).fail( function( response , status, error){
+		appViewModel.error( true );
+		appViewModel.errorMessage('Could load the restaurant');
 	});
+}
+
+function openInfoWindow( marker ) {
+	var find_marker;
+	for( var i = 0 ; i < markers.length ; i++ )
+	{
+		if( markers[ i ].position.lat().toFixed( 5 ) == marker.position.lat.toFixed( 5 )  && markers[ i ].position.lng().toFixed( 5 ) == marker.position.lng.toFixed( 5 ) )
+		{	
+			find_marker = markers[ i ];
+		}
+		markers[ i ].setMap( null );
+	}
+	find_marker.setMap( map );
+	addInfoWindow( find_marker , infowindow );
 }
 
 function addInfoWindow( marker , infowindow ) {
@@ -72,19 +102,17 @@ function stopCurrentMarker( infowindow , marker ){
 	}
 }
 
-
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
     center: myPlace,
     zoom: 12,
 	});
 	infowindow = new google.maps.InfoWindow();	
-	fetchRestaurant();
+	fetchRestaurant();	
 }
-
 function showMarkers() {
+	appViewModel.searchQuery('');
 	var boundsLimit = new google.maps.LatLngBounds();
-
 	for( var i = 0 ; i < markers.length ; i++ )
 	{
 		markers[ i ].setMap( map );
@@ -92,7 +120,6 @@ function showMarkers() {
 	}
 	map.fitBounds( boundsLimit );	
 }
-
 function hideMarkers() {
 //	console.log( markers );
 	for( var i = 0 ; i < markers.length ; i++ )
@@ -100,13 +127,36 @@ function hideMarkers() {
 		markers[ i ].setMap( null );
 	}	
 }
-var viewModel =  function(){
-	var self = this;
-	self.reslist = ko.observableArray( [1,2,3] );
+var appViewModel = {
+	list: ko.observableArray([]),
+	searchQuery: ko.observable(''),
+	error: ko.observable( false ),
+	errorMessage : ko.observable(''),
+	init: function(query){
+		for (var i in locations){
+			appViewModel.list.push(locations[i]);
+		}
+	},
+	searchingQuery: function(query){
+		appViewModel.list.removeAll();
+		//console.log( query );
+		for( var i = 0 ; i < markers.length ; i++ )
+		{
+			markers[ i ].setMap( null );
+		}
+		for( var i = 0 ; i < locations.length ; i++ )
+		{
+			if( locations[ i ].title.toLowerCase().indexOf( query.toLowerCase()) >=0 )
+			{
+				appViewModel.list.push( locations[ i ] );
+			}	
+		}	
+	},
 }
-ko.applyBindings( new viewModel() );
 
-
+ko.applyBindings( appViewModel );
+appViewModel.searchQuery.subscribe(appViewModel.searchingQuery);
+ 		
 
 
 
